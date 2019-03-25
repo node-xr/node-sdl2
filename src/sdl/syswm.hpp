5d857298dcc8e40e3d00890dd807548910c632db
Pras Velagapudi <psigen@gmail.com>
2019-03-25 03:19:41 -0400

    Added a fresh version of SDL that matches the linux build.

---
+++ src/sdl/syswm.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../util.hpp"
+#include <node_api.h>
+
+void create_sdl_syswm(napi_env env, napi_value exports);
+
+napi_value napi_SDL_GetWindowWMInfo(napi_env, napi_callback_info info);
