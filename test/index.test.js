5045dbfb24d3cfc903bc6e90481b7688242d7200
Pras Velagapudi <psigen@gmail.com>
2019-03-25 00:09:27 -0400

    Added constants file and unit test.

--- test/index.test.js
+++ test/index.test.js
@@ -5,5 +5,9 @@ describe('sdl', () => {
     expect(sdl).toBeDefined();
   });
 
+  it('includes SDL_INIT_* enum', () => {
+    expect(sdl.SDL_INIT_EVERYTHING).toEqual(0x0000f231);
+  });
+
   // TODO: add unit tests per function.
 });
