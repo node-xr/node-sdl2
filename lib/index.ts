b062f5ec01a49b4c2adbacd364b6ec096f4ce5f3
Pras Velagapudi <psigen@gmail.com>
2019-05-05 00:56:26 -0400

    Javascript compatibilit.

--- lib/index.ts
+++ lib/index.ts
@@ -7,4 +7,5 @@ const sdl = bindings('sdl2');
 Object.assign(sdl, constants);
 
 // Export the correctly typed binding.
-export default sdl as typeof constants & SDL;
+// FIXME: replace this syntax once node supports ES6 imports.
+export = sdl as typeof constants & SDL;
